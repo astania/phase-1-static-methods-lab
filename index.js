8a2a7acd6195bf56f918cee9b59b0f7e6db73611
+ <+>
2021-12-17 10:05:01 -0500

    commit tree f41d14b7ff7269e56caac9bed18cbfbaf2b5dcf1

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\w ]+/g, "")
+  }
+
+  static titleize(string){
+    let arr = []
+    let excludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    arr = string.split(' ')
+    arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1)
+    return arr.map(word => {
+      return excludeWords.includes(word) ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  }
 }
